fb59f90926818e3ff39f0cf85ebda221e85729b9
Min Kim <mk4555@nyu.edu>
2018-01-11 06:21:58 +0000

    automatically backed up by learn

--- db/migrate/001_create_costumes.rb
+++ db/migrate/001_create_costumes.rb
@@ -14,8 +14,7 @@ class CreateCostumes < ActiveRecord::Migration[4.2]
       t.integer :price
       t.string :size
       t.string :image_url
-      t.timestamps :created_at
-      t.timestamps :updated_at
+      t.timestamps 
     end
   end
 end
