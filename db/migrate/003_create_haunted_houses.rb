0dc79fd444c77faa2263f457a802232f510aca89
Min Kim <mk4555@nyu.edu>
2018-01-11 06:16:29 +0000

    automatically backed up by learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -9,7 +9,7 @@ class CreateHauntedHouses < ActiveRecord::Migration[4.2]
       t.boolean :family_friendly
       t.datetime :opening_time
       t.datetime :closing_time
-      t.string :long_description
+      t.string :description
     end
   end
 end
